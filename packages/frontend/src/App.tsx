49fb0328502620b9c653d6a1703ac09b7fddfbb6
Truong Nhat Anh <truongnhatanhw@gmail.com>
2024-08-15 12:20:03 +0700

    feat: add gen form

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,15 +4,19 @@ import EarlyAccessWrapper from "./layouts/EarlyAccessWrapper";
 import Gen from "./layouts/Gen";
 
 const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <EarlyAccessWrapper />,
+  //   children: [
+  //     {
+  //       path: "/",
+  //       element: <Gen />,
+  //     },
+  //   ],
+  // },
   {
     path: "/",
-    element: <EarlyAccessWrapper />,
-    children: [
-      {
-        path: "/",
-        element: <Gen />,
-      },
-    ],
+    element: <Gen />,
   },
 ]);
 
--- packages/frontend/src/layouts/Gen/index.tsx
+++ packages/frontend/src/layouts/Gen/index.tsx
@@ -1,7 +1,98 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
+const GenSchema = z.object({
+  topic: z.string().min(10, {
+    message: "Topic must be specific",
+  }),
+});
 
 const Gen = () => {
-  return <div>Gen</div>;
+  const form = useForm<z.infer<typeof GenSchema>>({
+    resolver: zodResolver(GenSchema),
+    defaultValues: {
+      topic: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof GenSchema>) => {
+    console.log(values);
+  };
+  return (
+    <div className="w-screen h-screen grid place-items-center">
+      <div className="w-8/12">
+        <Card>
+          <CardHeader>
+            <CardTitle>ThinkForce | Knowledge Fusion</CardTitle>
+            <CardDescription>
+              ThinkForce replicate the process of a human researcher, hence,
+              once you pressed the "Generate" button, it will take some time to
+              process the information <b>(5-8 minutes)</b>.
+              <br />
+              After that, you could download the generated content.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                  control={form.control}
+                  name="topic"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="topic">Topic</FormLabel>
+                      <Input
+                        placeholder="Enter a specific topic. Example: 'Comparision between combustion engine and electric motor'"
+                        {...field}
+                      />
+                      <FormDescription>
+                        Enter a specific topic you want to generate content for
+                      </FormDescription>
+                      <FormMessage>
+                        {form.formState.errors.topic?.message}
+                      </FormMessage>
+                    </FormItem>
+                  )}
+                />
+
+                <Button type="submit" className="mt-4">
+                  Generate
+                </Button>
+              </form>
+            </Form>
+            <div className="my-4">
+              <Separator />
+            </div>
+            <CardTitle>Result</CardTitle>
+            <CardDescription className="mt-3">
+              Your result will be displayed here.
+            </CardDescription>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Gen;
