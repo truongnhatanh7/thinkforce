15eea2b5150b6e0863e4021f91e9f32b84834c2a
Truong Nhat Anh <truongnhatanhw@gmail.com>
2024-08-22 19:07:13 +0700

    feat: new view doc flow

--- packages/frontend/src/layouts/Gen/MainForm/Job.tsx
+++ packages/frontend/src/layouts/Gen/MainForm/Job.tsx
@@ -3,17 +3,16 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { Tables } from "@/repo/database.types";
-import { supabase } from "@/supabase";
-import React, { useEffect, useState } from "react";
-import { marked } from "marked";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export interface JobProps {
   doc: Tables<"doc_meta">;
 }
 
 const Job: React.FC<JobProps> = ({ doc }) => {
-  const [isLoading, setIsLoading] = useState(false);
   const [progress, setProgress] = React.useState(0);
+  const navigate = useNavigate();
 
   const handleResolveProgress = () => {
     switch (doc?.status) {
@@ -45,59 +44,8 @@ const Job: React.FC<JobProps> = ({ doc }) => {
     return () => clearTimeout(timer);
   }, [doc]);
 
-  const handleGetPresignedUrl = async (runId: string) => {
-    setIsLoading(true);
-    const session = await supabase.auth.getSession();
-    const userId = session.data.session?.user.id;
-    const baseUrl = new URL(import.meta.env.VITE_SUPABASE_URL || "");
-    baseUrl.pathname = "/functions/v1/backend/gen/poll";
-    baseUrl.searchParams.append("userId", userId || "");
-    baseUrl.searchParams.append("runId", runId || "");
-    try {
-      const req = await fetch(baseUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${session.data.session?.access_token}`,
-        },
-      });
-      const res = await req.json();
-      setIsLoading(false);
-      return res.signedUrl;
-    } catch (err) {
-      setIsLoading(false);
-      console.error(err);
-      return "";
-    }
-  };
-
-  const handleGetDownloadLink = async () => {
-    const presignedUrl = await handleGetPresignedUrl(doc?.run_id || "");
-
-    // create a tag and click it
-    const mdReq = await fetch(presignedUrl);
-    const md = await mdReq.text();
-
-    const htmlString = await marked(md);
-    const blob = new Blob([htmlString], { type: "text/html" });
-    // Create an object URL for the Blob
-    const url = URL.createObjectURL(blob);
-
-    // Create a temporary anchor element
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "sample.html"; // Set the file name
-
-    // Programmatically click the anchor to trigger the download
-    a.click();
-
-    // Clean up by revoking the object URL
-    URL.revokeObjectURL(url);
-
-    // const a = window.document.createElement("a");
-    // a.href = url;
-    // a.download = doc?.file_name || "";
-    // a.click();
+  const handleNavigateToDocViewer = () => {
+    navigate("/viewer/" + encodeURIComponent(doc?.file_name));
   };
 
   return (
@@ -117,11 +65,10 @@ const Job: React.FC<JobProps> = ({ doc }) => {
 
             <Button
               className="flex items-center gap-1"
-              onClick={() => handleGetDownloadLink()}
-              disabled={doc?.status !== "completed" || isLoading}
+              onClick={() => handleNavigateToDocViewer()}
+              disabled={doc?.status !== "completed"}
             >
-              Download
-              {isLoading && <Spinner />}
+              Open
             </Button>
           </div>
         </div>
--- packages/frontend/src/layouts/Gen/Viewer/index.tsx
+++ packages/frontend/src/layouts/Gen/Viewer/index.tsx
@@ -1,12 +1,14 @@
 import { useGetDocQuery } from "@/api/useDocQuery";
 import { Spinner } from "@/components/spinner";
-import { Card, CardContent } from "@/components/ui/card";
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { marked } from "marked";
+import React, { useRef } from "react";
 import Markdown from "react-markdown";
 import {
   LoaderFunction,
   LoaderFunctionArgs,
-  useLoaderData
+  useLoaderData,
 } from "react-router-dom";
 
 export const viewerDocLoader: LoaderFunction = async ({
@@ -23,13 +25,69 @@ export const viewerDocLoader: LoaderFunction = async ({
 interface ViewerProps {}
 
 const Viewer: React.FC<ViewerProps> = () => {
+  const docDataRef = useRef<HTMLDivElement>(null);
   const fileName = useLoaderData();
 
   const doc = useGetDocQuery((fileName as string) || "");
 
+  // const handleGetPresignedUrl = async (runId: string) => {
+  //   setIsLoading(true);
+  //   const session = await supabase.auth.getSession();
+  //   const userId = session.data.session?.user.id;
+  //   const baseUrl = new URL(import.meta.env.VITE_SUPABASE_URL || "");
+  //   baseUrl.pathname = "/functions/v1/backend/gen/poll";
+  //   baseUrl.searchParams.append("userId", userId || "");
+  //   baseUrl.searchParams.append("runId", runId || "");
+  //   try {
+  //     const req = await fetch(baseUrl, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${session.data.session?.access_token}`,
+  //       },
+  //     });
+  //     const res = await req.json();
+  //     setIsLoading(false);
+  //     return res.signedUrl;
+  //   } catch (err) {
+  //     setIsLoading(false);
+  //     console.error(err);
+  //     return "";
+  //   }
+  // };
+
+  const handleGetDownloadLink = async () => {
+    const htmlString = await marked(doc?.data as string);
+    const blob = new Blob([htmlString], { type: "text/html" });
+    // Create an object URL for the Blob
+    const url = URL.createObjectURL(blob);
+
+    // Create a temporary anchor element
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "sample.html"; // Set the file name
+
+    // Programmatically click the anchor to trigger the download
+    a.click();
+
+    // Clean up by revoking the object URL
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <div className="w-full h-full">
       <Card className="rounded-none w-full h-full">
+        <CardHeader>
+          <Button
+            onClick={() => {
+              handleGetDownloadLink();
+            }}
+            className="flex gap-1"
+            disabled={doc.isLoading}
+          >
+            {doc.isLoading ? <Spinner /> : "Download as HTML"}
+          </Button>
+        </CardHeader>
         <CardContent>
           {doc.isLoading ? (
             <div className="w-full h-screen grid place-items-center">
@@ -38,7 +96,9 @@ const Viewer: React.FC<ViewerProps> = () => {
               </div>
             </div>
           ) : (
-            <Markdown className="markdown">{doc.data as string}</Markdown>
+            <div className="" ref={docDataRef}>
+              <Markdown className="markdown">{doc.data as string}</Markdown>
+            </div>
           )}
         </CardContent>
       </Card>
